200923b5b29b5729c8a85cbf8ae4a92ada258d7a
maruhxn <a23686397@gmail.com>
2023-01-26 04:18:25 +0900

    delete dist

--- server/dist/middleware/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const authMiddleware = (req, res, next) => {
-    if (!req.session.loggedInUser) {
-        return res.json({ isAuth: false, error: true });
-    }
-    next();
-};
-exports.default = authMiddleware;
